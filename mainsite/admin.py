7effa8e449fa95e9639cfb9bfb370bf9c082194d
Richard <skynet.tw@gmail.com>
2020-11-24 15:19:21 +0800

    first commit

---
+++ mainsite/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from mainsite.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+
